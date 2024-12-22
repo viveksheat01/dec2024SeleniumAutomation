99e0d7b118bcdbc191f3f4681d275f05538739ed
viveksheat01 <viveksheat01@gmail.com>
2024-12-22 14:40:52 +0530

    commit code vivek

--- src/main/java/com/qa/opencart/pages/Login.java
+++ src/main/java/com/qa/opencart/pages/Login.java
@@ -3,8 +3,13 @@ package com.qa.opencart.pages;
 public class Login {
 	
 	int i =10;
+	
 	public void main() {
 		System.out.println("vivek-------main");
+	}
+	
+	public void doLoogin() {
+		System.out.println("login ----vivek");
 	}
 
 }
