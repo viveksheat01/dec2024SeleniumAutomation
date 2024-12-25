fa2f4b6113a4336af84814e84602ceb888106c25
viveksheat01 <viveksheat01@gmail.com>
2024-12-25 14:53:36 +0530

    test

--- src/main/java/com/qa/opencart/pages/SamplePage.java
+++ src/main/java/com/qa/opencart/pages/SamplePage.java
@@ -2,12 +2,8 @@ package com.qa.opencart.pages;
 
 public class SamplePage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
 		
 		int i=10;
-		System.out.println(i);
-	}
-
+	
 }
